655d2e30e875a3a2c94c9b28b751526a7656dd63
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-04-15 21:25:39 +0600

    store name of google user

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ passport.use(new GoogleStrategy({
  },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile)
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  User.findOrCreate({ googleId: profile.id, name: profile.displayName }, function (err, user) {
     return cb(err, user);
   })
 }
